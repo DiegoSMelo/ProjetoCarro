0e43c94fde6c5e4d1ea721ba3511f83a6a8f9f45
Diego Melo <diosmelo@gmail.com>
2015-11-11 22:29:33 -0300

    Comentários

--- app/src/main/java/com/example/diegomelo/projetocarro/CarroActivity.java
+++ app/src/main/java/com/example/diegomelo/projetocarro/CarroActivity.java
@@ -19,35 +19,68 @@ public class CarroActivity extends AppCompatActivity implements ListaCarroFragme
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carro);
 
+        /*
+        Classe responsável por gerenciar a funcionalidade de arrastar o layout pra esquerda e direita.
+        Controle de troca de fragments.
+         */
         ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
         viewPager.setAdapter(new PaginasAdapter(getSupportFragmentManager()));
 
+        /*
+          Gerencia os eventos das tabs
+          Add uma ViewPager para transmitir eventos para o TabLayout.
+        */
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
     }
 
-
+    /*
+    Método executado ao clicar em um carro da lista.
+     */
     @Override
     public void voceClicouNoCarro(Carro c) {
+        /*
+        Verifica se o aparelho é fone ou tablet.
+         */
         if (getResources().getBoolean(R.bool.fone)) {
+            /*
+            Caso seja fone, ele inicia a activity de detalhe, passando com o parâmetro o objeto do carro a ser detalhado.
+            Nesse caso, o fragment é implementado em outra activity.
+             */
             Intent it = new Intent(this, DetalheCarroActivity.class);
             it.putExtra("carro", c);
             startActivity(it);
         }else{
+            /*
+            Caso seja tablet, não haverá mudança de activity para implementar a fragment.
+             */
 
+            /*
+            ?
+
+             */
             DetalheCarroFragment detalheCarroFragment = DetalheCarroFragment.novaInstancia(c);
-
+            /*
+            Substitui o que está sendo exibido no layout com id detalhe pelo fragment que está sendo passado.
+             */
             getSupportFragmentManager().beginTransaction().replace(R.id.detalhe, detalheCarroFragment).commit();
         }
     }
 
 
+    /*
+     O PageAdapter é uma classe que serve para preencher páginas dentro de um ViewPager.
+     O FragmentPagerAdapter implementa um PageAdapter, representando cada página como um fragment.
+     */
     private class PaginasAdapter extends FragmentPagerAdapter{
 
         public PaginasAdapter(FragmentManager fragmentManager){
             super(fragmentManager);
         }
 
+        /*
+        Retorna o Fregment específico para a posição passada como parâmetro.
+         */
         @Override
         public Fragment getItem(int position) {
             if (position == 0){
@@ -57,18 +90,24 @@ public class CarroActivity extends AppCompatActivity implements ListaCarroFragme
             }
         }
 
+        /*
+        Retorna o número de views disponiveis.
+         */
         @Override
         public int getCount() {
             return 2;
         }
 
+        /*
+        Método que é chamado pela ViewPager para descrever títulos para as páginas.
+         */
         @Override
         public CharSequence getPageTitle(int position) {
 
             if (position == 0){
-                return "Lista de carros";
+                return getBaseContext().getString(R.string.title_listaDeCarros);
             }else{
-                return "Meus favoritos";
+                return getBaseContext().getString(R.string.title_meusFavoritos);
             }
 
         }
